5b5fa06d9187410dceebde3bd21d1d20149376fb
xgnit <xgnit@xgnit.com>
2020-02-11 16:20:30 +0100

    path generator

--- Map.py
+++ Map.py
@@ -1,6 +1,7 @@
 
 from Utils import *
-
+import numpy as np
+from matplotlib import pyplot as plt
 
 class Map:
     def __init__(self):
@@ -18,12 +19,44 @@ class Map:
             for j in range(100):
                 image_load[i,j] = (200, 0, 0)
 
-        self.bg.show()
+        # self.bg.show()
+        self.path_generator()
         print(1)
 
 
     def path_generator(self):
 
+        n= 5
+
+        x = np.random.randint(0, 50, n)
+        y = np.random.randint(0, 50, n)
+
+        ##computing the (or a) 'center point' of the polygon
+        center_point = [np.sum(x) / n, np.sum(y) / n]
+
+        angles = np.arctan2(x - center_point[0], y - center_point[1])
+
+        ##sorting the points:
+        sort_tups = sorted([(i, j, k) for i, j, k in zip(x, y, angles)], key=lambda t: t[2])
+
+        ##making sure that there are no duplicates:
+        if len(sort_tups) != len(set(sort_tups)):
+            raise Exception('two equal coordinates -- exiting')
+
+        x, y, angles = zip(*sort_tups)
+        x = list(x)
+        y = list(y)
+
+        ##appending first coordinate values to lists:
+        x.append(x[0])
+        y.append(y[0])
+
+        fig, ax = plt.subplots()
+
+        ax.plot(x, y, label='{}'.format(n))
+        fig.show()
+
+        print(1)
         pass
 
 
